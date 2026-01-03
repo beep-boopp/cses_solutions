bc05012def9b9ffdcca79db995e118d3b79d266c
beep-boopp <percy38621@gmail.com>
2026-01-03 18:08:29 +0530

    dp

--- coin1.cpp
+++ coin1.cpp
@@ -1,42 +1,42 @@
-#include<bits/stdc++.h>
-using namespace std;
+    #include<bits/stdc++.h>
+    using namespace std;
 
-#define ll long long
-
-void solve()
-{
-    ll n,x;
-    cin>>n>>x;
-
-    vector<ll> c(n);
-    for(ll i =0;i<n;i++)
+    #define ll long long
+    const ll MOD = 1e9 + 7;
+    void solve()
     {
-        cin>>c[i];
-    }
-    vector<ll> dp(x+1,0);
+        ll n,x;
+        cin>>n>>x;
 
-    //i goes from 1 to n
-    //j goes from 0 to n-1
-    dp[0]=1;
-    for(ll i =1;i<=x;i++)
-    {
-        for(ll j=0;j<n;j++)
+        vector<ll> c(n);
+        for(ll i =0;i<n;i++)
         {
-            if(c[j]<=i)
+            cin>>c[i];
+        }
+        vector<ll> dp(x+1,0);
+
+        //i goes from 1 to n
+        //j goes from 0 to n-1
+        dp[0]=1;
+        for(ll i =1;i<=x;i++)
+        {
+            for(ll j=0;j<n;j++)
             {
-            dp[i]+=dp[i-c[j]];
+                if(c[j]<=i)
+                {
+                dp[i] = (dp[i] + dp[i - c[j]]) % MOD;
+                }
             }
         }
+        cout<<(dp[x])<<endl;
+
     }
-    cout<<(dp[x]<1e9? dp[x]:-1)<<endl;
 
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-   solve(); 
-    return 0;
-}
+    int main()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+    solve(); 
+        return 0;
+    }
