a038f44ff22532c7f926c41a280dc7aa3ba7abb9
beep-boopp <percy38621@gmail.com>
2026-01-03 18:06:08 +0530

    dp

---
+++ coin1.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solve()
+{
+    ll n,x;
+    cin>>n>>x;
+
+    vector<ll> c(n);
+    for(ll i =0;i<n;i++)
+    {
+        cin>>c[i];
+    }
+    vector<ll> dp(x+1,0);
+
+    //i goes from 1 to n
+    //j goes from 0 to n-1
+    dp[0]=1;
+    for(ll i =1;i<=x;i++)
+    {
+        for(ll j=0;j<n;j++)
+        {
+            if(c[j]<=i)
+            {
+            dp[i]+=dp[i-c[j]];
+            }
+        }
+    }
+    cout<<(dp[x]<1e9? dp[x]:-1)<<endl;
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+   solve(); 
+    return 0;
+}
